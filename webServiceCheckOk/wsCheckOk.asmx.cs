e2768a2a48efd13d4ec28426f31996838fdf41c3
Domingos Ribeiro <domingos.ribeiro@credify.com.br>
2016-03-11 11:28:16 -0300

    Commit bTest1

--- webServiceCheckOk/wsCheckOk.asmx.cs
+++ webServiceCheckOk/wsCheckOk.asmx.cs
@@ -15,7 +15,7 @@ using webServiceCheckOk.Model.ProdutosModel;
 namespace webServiceCheckOk
 {
     /// <summary>
-    /// Branch Local
+    /// Branch Teste1
     /// AUTHOR: Domingos Ribeiro <dofari.dfr@gmail.com>
     /// DOFARI: Novo teste 11.03.2016
     /// </summary>
